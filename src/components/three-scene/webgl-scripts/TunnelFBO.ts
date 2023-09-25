3a317db3bf0bba88da25964ae1b4e47a190d3a32
MariusBallot <ballot.marius@gmail.com>
2023-09-25 13:54:39 +0200

    window error fixed

--- src/components/three-scene/webgl-scripts/TunnelFBO.ts
+++ src/components/three-scene/webgl-scripts/TunnelFBO.ts
@@ -1,57 +1,61 @@
-import * as THREE from "three"
-import FBOVert from "./shaders/FBOVert.glsl"
-import FBOFrag from "./shaders/FBOFrag.glsl"
+import * as THREE from "three";
+import FBOVert from "./shaders/FBOVert.glsl";
+import FBOFrag from "./shaders/FBOFrag.glsl";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 class TunnelFBO {
     private scene: THREE.Scene | undefined;
     private GLTFLoader: GLTFLoader;
-    public frameTexture: THREE.WebGLRenderTarget;
+    public frameTexture: THREE.WebGLRenderTarget | undefined;
     private texLoader: THREE.TextureLoader;
-    private FBOShader: THREE.ShaderMaterial
+    private FBOShader: THREE.ShaderMaterial | undefined;
 
     constructor() {
-        this.GLTFLoader = new GLTFLoader()
-        this.texLoader = new THREE.TextureLoader()
-        this.frameTexture = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
-        this.FBOShader = new THREE.ShaderMaterial({
-            uniforms: {
-                u_fboTex: {
-                    value: this.frameTexture.texture,
-                },
-                u_res: {
-                    value: new THREE.Vector2(window.innerWidth, window.innerHeight)
-                },
-            },
-            vertexShader: FBOVert,
-            fragmentShader: FBOFrag,
-        })
+        this.GLTFLoader = new GLTFLoader();
+        this.texLoader = new THREE.TextureLoader();
     }
 
     init = (scene: THREE.Scene) => {
         this.scene = scene;
 
-        this.GLTFLoader.load("/assets/models/mirror-portal.glb", glb => {
-            console.log(glb.scene)
-            glb.scene.rotateY(-Math.PI / 2)
-            glb.scene.traverse(child => {
+        // Create frameTexture and FBOShader in init method when it's safe to access window properties
+        this.frameTexture = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
+        this.FBOShader = new THREE.ShaderMaterial({
+            uniforms: {
+                u_fboTex: {
+                    value: this.frameTexture?.texture || null,
+                },
+                u_res: {
+                    value: new THREE.Vector2(window.innerWidth, window.innerHeight),
+                },
+            },
+            vertexShader: FBOVert,
+            fragmentShader: FBOFrag,
+        });
+
+        this.GLTFLoader.load("/assets/models/mirror-portal.glb", (glb) => {
+            console.log(glb.scene);
+            glb.scene.rotateY(-Math.PI / 2);
+            glb.scene.traverse((child) => {
                 if (child instanceof THREE.Mesh) {
-                    if (child.name === "mirror-fbo")
-                        child.material = this.FBOShader
+                    if (child.name === "mirror-fbo") child.material = this.FBOShader;
                     if (child.name === "mirror-struc")
                         child.material = new THREE.MeshMatcapMaterial({
-                            matcap: this.texLoader.load("assets/textures/black-metal-matcap.png")
-                        })
+                            matcap: this.texLoader.load("assets/textures/black-metal-matcap.png"),
+                        });
                 }
-            })
-            this.scene?.add(glb.scene)
-        })
-    }
+            });
+            this.scene?.add(glb.scene);
+        });
+    };
 
     onResize = () => {
-        this.FBOShader.uniforms.u_res.value = new THREE.Vector2(window.innerWidth, window.innerHeight)
-    }
+        if (this.FBOShader && this.frameTexture) {
+            this.FBOShader.uniforms.u_res.value = new THREE.Vector2(window.innerWidth, window.innerHeight);
+            this.frameTexture.setSize(window.innerWidth, window.innerHeight);
+        }
+    };
 }
 
-const _instance = new TunnelFBO()
-export default _instance
+const _instance = new TunnelFBO();
+export default _instance;
--- src/utils/RAF.ts
+++ src/utils/RAF.ts
@@ -1,10 +1,11 @@
 class RAF {
     private callbacks: { name: string; callback: () => void }[];
+    private requestId: number | undefined;
 
     constructor() {
         this.bind();
         this.callbacks = [];
-        this.render()
+        this.render();
     }
 
     subscribe(name: string, callback: () => void) {
@@ -15,29 +16,31 @@ class RAF {
     }
 
     unsubscribe(name: string) {
-        this.callbacks.forEach((item, i) => {
-            if (item.name === name) {
-                this.callbacks.splice(i, 1);
-            }
-        });
+        this.callbacks = this.callbacks.filter((item) => item.name !== name);
     }
 
-    render() {
-        if (window !== undefined) {
-            window.requestAnimationFrame(this.render);
+    render = () => {
+        if (typeof window !== 'undefined') {
+            this.requestId = window.requestAnimationFrame(this.render);
         }
         this.callbacks.forEach((item) => {
             item.callback();
         });
-    }
+    };
 
     private bind() {
         this.subscribe = this.subscribe.bind(this);
         this.unsubscribe = this.unsubscribe.bind(this);
         this.render = this.render.bind(this);
     }
+
+    stop() {
+        if (typeof window !== 'undefined' && this.requestId !== undefined) {
+            window.cancelAnimationFrame(this.requestId);
+            this.requestId = undefined;
+        }
+    }
 }
 
-
-const _instance = new RAF()
-export default _instance
+const _instance = new RAF();
+export default _instance;
